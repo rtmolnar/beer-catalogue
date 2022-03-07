20bcaa28d5cf122074d3ff193475e3ce49a66c1c
rtmolnar <raphaeltmolnar@gmail.com>
2022-03-07 22:43:36 +0100

    feat: initial commit
    Project template taken from https://start.spring.io/

---
+++ src/main/java/com/hauffe/test/beer/catalogue/BeerCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.hauffe.test.beer.catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hauffe/test/beer/catalogue/BeerCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hauffe.test.beer.catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
